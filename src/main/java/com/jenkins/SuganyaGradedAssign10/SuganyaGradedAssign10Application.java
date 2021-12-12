548c43fe60587a9d9de0a6b54a7a7cb9aa0be0fe
Suganya S <s_suganya@hcl.com>
2021-12-12 16:39:21 +0530

    Initial Assignment10

---
+++ src/main/java/com/jenkins/SuganyaGradedAssign10/SuganyaGradedAssign10Application.java
@@ -0,0 +1,14 @@
+package com.jenkins.SuganyaGradedAssign10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuganyaGradedAssign10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuganyaGradedAssign10Application.class, args);
+		System.out.println("Heelo Dev-Ops");
+	}
+
+}
---
+++ src/test/java/com/jenkins/SuganyaGradedAssign10/SuganyaGradedAssign10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.SuganyaGradedAssign10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuganyaGradedAssign10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
