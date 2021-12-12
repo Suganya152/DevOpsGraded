31ef2102d3f2d5d66df024f577ecdd86ee5f30f9
Suganya S <s_suganya@hcl.com>
2021-12-12 21:18:55 +0530

    Initial Assignment10

--- src/main/java/com/jenkins/SuganyaGradedAssign10/SuganyaGradedAssign10Application.java
+++ src/main/java/com/jenkins/SuganyaGradedAssign10/SuganyaGradedAssign10Application.java
@@ -1,4 +1,4 @@
-package com.jenkins.SuganyaGradedAssign10;
+package com.jenkins.suganyagradedassign10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,7 @@ public class SuganyaGradedAssign10Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuganyaGradedAssign10Application.class, args);
-		System.out.println("Heelo Dev-Ops");
+		System.out.println("Hello Dev-Ops");
 	}
 
 }
